c359bc1d480fa0e36a46320a98e4e6f86360c0f3
baae29 <68650006+baae29@users.noreply.github.com>
2022-05-16 20:03:22 -0500

    primero

---
+++ app/src/main/java/fisei/edu/alexis_acosta/Segunda_Pantalla.java
@@ -0,0 +1,14 @@
+package fisei.edu.alexis_acosta;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Segunda_Pantalla extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segunda_pantalla);
+    }
+}
---
+++ app/src/main/java/fisei/edu/alexis_acosta/Tercera_Pantalla.java
@@ -0,0 +1,14 @@
+package fisei.edu.alexis_acosta;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tercera_Pantalla extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tercera_pantalla);
+    }
+}
