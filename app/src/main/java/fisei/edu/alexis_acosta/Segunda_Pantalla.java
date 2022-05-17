22f2568d50ea547cb75c198e5769cf0041c43a80
baae29 <68650006+baae29@users.noreply.github.com>
2022-05-16 20:59:22 -0500

    Final

--- app/src/main/java/fisei/edu/alexis_acosta/Segunda_Pantalla.java
+++ app/src/main/java/fisei/edu/alexis_acosta/Segunda_Pantalla.java
@@ -2,7 +2,9 @@ package fisei.edu.alexis_acosta;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Segunda_Pantalla extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class Segunda_Pantalla extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segunda_pantalla);
+    }
+    public void onClick_Tercera(View view){
+        Intent intent = new Intent(Segunda_Pantalla.this, Tercera_Pantalla.class);
+        startActivity(intent);
     }
 }
